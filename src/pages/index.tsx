f18bad8b0ed7b04c6fca61f1ff02220db31e7af6
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2023-08-18 06:38:57 -0700

    add pages and styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Player } from "@/components"
+
+const VideoPlayer = () => {
+    return (
+        <>
+            <Player src="/video.mp4"/>
+        </>
+    )
+}
+
+export default VideoPlayer
